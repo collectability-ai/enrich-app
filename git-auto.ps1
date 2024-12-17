8e44ea69d87d532178cfccb4a41f46ce02a7606a
darrelbrownjr <operations@collectability.ai>
2024-12-16 17:11:31 -0700

    v1.3.2: fixed search and payment routes - removed useless json file

--- git-auto.ps1
+++ git-auto.ps1
@@ -1,8 +1,8 @@
 # Script to automate Git commit, push, and versioning to the development branch
 
 # Set version number and description
-$version = "v1.3.1"
-$description = "fixed search and payment routes"
+$version = "v1.3.2"
+$description = "fixed search and payment routes - removed useless json file"
 
 # Add all changes
 git add .
