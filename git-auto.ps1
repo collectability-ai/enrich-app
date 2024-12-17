8daec7b286b7d6a854f5bdcc9a2e9ed609e2506f
darrelbrownjr <operations@collectability.ai>
2024-12-17 10:13:51 -0700

    v1.3.4: updated amplify build scripts for testing

--- git-auto.ps1
+++ git-auto.ps1
@@ -1,8 +1,8 @@
 # Script to automate Git commit, push, and versioning to the development branch
 
 # Set version number and description
-$version = "v1.3.3"
-$description = "updated amplify build scripts"
+$version = "v1.3.4"
+$description = "updated amplify build scripts for testing"
 
 # Add all changes
 git add .
